720f8b82ff55d45465b379ca6711aabeb5eceaa7
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-10-12 12:40:26 -0600

    navbar

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -5,6 +5,7 @@ import MyOrder from "../MyOrder";
 import MyOrders from "../MyOrders";
 import NotFound from "../NotFound";
 import SignIn from "../SignIn";
+import Navbar from "../../components/Navbar";
 import "./style.css";
 
 const AppRoutes = () => {
@@ -24,6 +25,7 @@ const App = () => {
     <>
       <BrowserRouter>
         <AppRoutes />
+        <Navbar />
       </BrowserRouter>
     </>
   );
