355b99dfd73e923bceae13a72254bc041ff1d00c
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-10-13 14:41:12 -0600

    API consumption

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,23 +1,23 @@
-const Card = () => {
+const Card = (data) => {
   return (
     // bg-slate-800
-    <div className=" cursor-pointer w-56 h-60 rounded-lg">
+    <div className="cursor-pointer w-56 h-60 rounded-lg">
       <figure className="relative mb-2 w-full h-4/5">
         <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
-          Technology
+          {data.data.category.name}
         </span>
         <img
           className="w-full h-full object-cover rounded-lg"
-          src="https://images.pexels.com/photos/4887246/pexels-photo-4887246.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="headphones"
+          src={data.data.images[0]}
+          alt={data.data.title}
         />
         <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full text-slate-700 m-2 p-2">
           +
         </div>
       </figure>
       <p className="flex justify-between">
-        <span className="text-sm font-light">Headphones</span>
-        <span className="text-lg font-medium">$300</span>
+        <span className="text-sm font-light m-1">{data.data.title}</span>
+        <span className="text-lg font-medium">{data.data.price}</span>
       </p>
     </div>
   );
