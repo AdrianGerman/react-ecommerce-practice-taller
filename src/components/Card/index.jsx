a953dc169f3c655ccd239d5e43fe3c760e909a0a
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-10-15 20:31:14 -0600

    recovering productDetail

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -5,9 +5,18 @@ import { ShoppingCartContext } from "../../context";
 
 const Card = (data) => {
   const context = useContext(ShoppingCartContext);
+
+  const showProduct = (productDetail) => {
+    context.openProductDetail();
+    context.setProductToShow(productDetail);
+  };
+
   return (
     // bg-slate-800
-    <div className="cursor-pointer w-56 h-60 rounded-lg">
+    <div
+      className="cursor-pointer w-56 h-60 rounded-lg"
+      onClick={() => showProduct(data.data)}
+    >
       <figure className="relative mb-2 w-full h-4/5">
         <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
           {data.data.category.name}
