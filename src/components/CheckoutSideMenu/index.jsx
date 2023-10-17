adad0e57afa06912ebc8b4b3197c44e0bd07eff8
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-10-16 20:49:59 -0600

    OrderCard

--- src/components/CheckoutSideMenu/index.jsx
+++ src/components/CheckoutSideMenu/index.jsx
@@ -1,9 +1,11 @@
 import { useContext } from "react";
 import { XMarkIcon } from "@heroicons/react/24/solid";
 import { ShoppingCartContext } from "../../context";
+import OrderCard from "../OrderCard";
 
 const CheckoutSideMenu = () => {
   const context = useContext(ShoppingCartContext);
+  // console.log(context.cartProducts);
 
   return (
     <aside
@@ -15,6 +17,16 @@ const CheckoutSideMenu = () => {
         <div onClick={() => context.closeCheckoutSideMenu()}>
           <XMarkIcon className="h-6 w-6 text-red-500 cursor-pointer" />
         </div>
+      </div>
+      <div className="px-6">
+        {context.cartProducts.map((product) => (
+          <OrderCard
+            key={product.id}
+            title={product.title}
+            imageUrl={product.images}
+            price={product.price}
+          />
+        ))}
       </div>
     </aside>
   );
