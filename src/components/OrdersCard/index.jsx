9515a187b93f03b5ea8ef2362abf47aefcdb4608
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-10-18 11:53:58 -0600

    icons and shadow design orders

--- src/components/OrdersCard/index.jsx
+++ src/components/OrdersCard/index.jsx
@@ -1,18 +1,26 @@
 import { ChevronRightIcon } from "@heroicons/react/24/solid";
+import { CalendarDaysIcon } from "@heroicons/react/24/solid";
+import { ShoppingBagIcon } from "@heroicons/react/24/solid";
 
 const OrdersCard = (props) => {
   const { totalPrice, totalProducts } = props;
 
   return (
-    <div className="flex justify-between items-center mb-4 border border-white rounded-lg p-4 w-80">
+    <div className="flex justify-between items-center mb-4 border border-purple-900 rounded-lg p-4 w-80 bg-black-font-divs shadow-2xl">
       <p className="flex justify-between w-full">
         <div className="flex flex-col">
-          <span className="font-light">01.02.2023</span>
-          <span className="font-light">{totalProducts} productos</span>
+          <span className="font-light flex items-center justify-center">
+            <CalendarDaysIcon className="h-4 w-4 mr-2 " />
+            01.02.2023
+          </span>
+          <span className="font-light flex items-center justify-center">
+            <ShoppingBagIcon className="h-4 w-4 mx-2" />
+            {totalProducts} productos
+          </span>
         </div>
         <div className="flex items-center gap-2">
           <span className="font-medium text-2xl">${totalPrice}</span>
-          <ChevronRightIcon className="h-6 w-6 text-white cursor-pointer" />
+          <ChevronRightIcon className="h-6 w-6 text-white" />
         </div>
       </p>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ export default {
     extend: {
       colors: {
         "black-font": "#242424",
+        "black-font-divs": "#343434",
+      },
+      boxShadow: {
+        "3xl": "0 25px 50px -12px rgba(90, 90, 90, 0.3)",
       },
     },
   },
