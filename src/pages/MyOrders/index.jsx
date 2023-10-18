cbfd8616ffa27d52bc9cc6e98069fdeb5703d3ea
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-10-18 09:25:41 -0600

    order list

--- src/pages/MyOrders/index.jsx
+++ src/pages/MyOrders/index.jsx
@@ -1,9 +1,31 @@
+import { useContext } from "react";
 import Layout from "../../components/Layout";
+import OrdersCard from "../../components/OrdersCard";
+import { ShoppingCartContext } from "../../context";
+import { Link } from "react-router-dom";
+import { ChevronRightIcon } from "@heroicons/react/24/solid";
 
 function MyOrders() {
+  const context = useContext(ShoppingCartContext);
   return (
     <>
-      <Layout>MyOrders</Layout>
+      <Layout>
+        <div className="flex w-80 items-center relative justify-center mb-6">
+          <Link to="/my-order" className="absolute right-0">
+            <ChevronRightIcon className="h-6 w-6 text-white cursor-pointer" />
+          </Link>
+          <h1>My Orders</h1>
+        </div>
+        {context.order.map((order, index) => {
+          <Link key={index} to={`/my-orders/${order.id}`}>
+            <OrdersCard
+              totalPrice={order.totalPrice}
+              totalProducts={order.totalProducts}
+            />
+            ;
+          </Link>;
+        })}
+      </Layout>
     </>
   );
 }
