67a3d7b2d98ae3a0a78558b9366b25c7ead922b0
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-10-19 10:48:28 -0600

    search by title front

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,30 +1,36 @@
-import { useState, useEffect } from "react";
+import { useContext } from "react";
 
 import Layout from "../../components/Layout";
 import Card from "../../components/Card";
 import ProductDetail from "../../components/ProductDetail";
+import { ShoppingCartContext } from "../../context";
+
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 function Home() {
-  const [items, setItems] = useState(null);
-
-  useEffect(() => {
-    fetch("https://api.escuelajs.co/api/v1/products")
-      .then((response) => response.json())
-      .then((data) => setItems(data));
-  }, []);
+  const context = useContext(ShoppingCartContext);
 
   return (
-    <>
-      <Layout>
-        Home
-        <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
-          {items?.map((item) => (
-            <Card key={item.id} data={item} />
-          ))}
-        </div>
-        <ProductDetail />
-      </Layout>
-    </>
+    <Layout>
+      <div className="flex w-80 items-center relative justify-center mb-6">
+        <h1 className="font-medium text-xl">Productos exclusivos</h1>
+      </div>
+      <div className="flex items-center justify-center p-4 ">
+        <MagnifyingGlassIcon className="h-6 w-6 mb-4 mr-2" />
+        <input
+          type="text"
+          placeholder="Busca un producto"
+          className="rounded-lg border border-slate-400 w-80 p-4 mb-4 bg-black-font focus:outline-none"
+          onChange={(event) => context.setSearchByTitle(event.target.value)}
+        />
+      </div>
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {context.items?.map((item) => (
+          <Card key={item.id} data={item} />
+        ))}
+      </div>
+      <ProductDetail />
+    </Layout>
   );
 }
 
--- src/pages/MyOrders/index.jsx
+++ src/pages/MyOrders/index.jsx
@@ -3,7 +3,6 @@ import Layout from "../../components/Layout";
 import OrdersCard from "../../components/OrdersCard";
 import { ShoppingCartContext } from "../../context";
 import { Link } from "react-router-dom";
-import { ChevronRightIcon } from "@heroicons/react/24/solid";
 
 function MyOrders() {
   const context = useContext(ShoppingCartContext);
@@ -11,9 +10,6 @@ function MyOrders() {
     <>
       <Layout>
         <div className="flex w-80 items-center relative justify-center mb-6">
-          <Link to="/my-order" className="absolute right-0">
-            <ChevronRightIcon className="h-6 w-6 text-white cursor-pointer" />
-          </Link>
           <h1 className="font-medium text-xl">My Orders</h1>
         </div>
         {context.order.map((order, index) => (
